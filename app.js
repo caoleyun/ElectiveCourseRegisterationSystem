0ef27db412ec28f17a6127bd02e767f2e0c34c4f
caoleyun <244814637@qq.com>
2020-04-07 18:27:16 +0800

    admin-初步路由+学生信息导入

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var mongoose = require('mongoose');
+var adminCtrl = require("./controllers/adminCtrl.js");
+
+
+//创建app
+var app = express();
+//链接数据库，斜杠后面是数据库的名字
+mongoose.connect('mongodb://localhost/ElectiveCourseRegisterationSystem');
+
+//设置模板引擎
+app.set("view engine","ejs");
+
+//中间件，路由清单
+app.get ("/admin"				,adminCtrl.showAdminDashborad);	//管理员界面
+app.get ("/admin/student"		,adminCtrl.showAdminStudent);	//管理员界面
+app.get ("/admin/student/import",adminCtrl.showAdminStudentImport);	//管理员界面
+app.post("/admin/student/import",adminCtrl.doAdminStudentImport);	//管理员界面
+app.get ("/admin/course"		,adminCtrl.showAdminCourse);	//管理员界面
+app.get ("/admin/report"		,adminCtrl.showAdminReport);	//管理员界面
+// app.get ("/student"		        ,adminCtrl.getAllStudent);		//得到所有学生
+ 
+
+
+//静态资源文件
+app.use(express.static("public"));
+
+//设置一个404页面
+app.use(function(req,res){
+	res.send("你好，你的页面不存在");
+});
+
+app.listen(3000);
+console.log("程序已经运行在3000端口");
---
+++ controllers/adminCtrl.js
@@ -0,0 +1,128 @@
+var formidable = require('formidable');
+var path = require("path");
+var fs = require("fs");
+// var url = require("url");
+var xlsx = require('node-xlsx');
+var Student = require("../models/Student.js");
+
+exports.showAdminDashborad = function(req,res){
+	res.render("admin/index",{
+		page : "index"
+	});
+}
+
+exports.showAdminStudent = function(req,res){
+	res.render("admin/student",{
+		page : "student"
+	});
+}
+
+// exports.showAdminStudentExport = function(req,res){
+// 	res.render("admin/student/export",{
+// 		page : "student"
+// 	});
+// }
+
+
+exports.showAdminStudentImport = function(req,res){
+	res.render("admin/student/import",{
+		page : "student"
+	});
+}
+
+
+exports.showAdminCourse = function(req,res){
+	res.render("admin/course",{
+		page : "course"
+	});
+}
+
+exports.showAdminReport = function(req,res){
+	res.render("admin/report",{
+		page : "report"
+	});
+}
+
+
+//执行表格的上传
+exports.doAdminStudentImport = function(req,res){
+	var form = new formidable.IncomingForm();
+	form.uploadDir = "./uploads";
+	form.keepExtensions = true;
+    form.parse(req, function(err, fields, files) {
+    	if(!files.studentexcel.name){
+    		res.send("对不起，请上传文件！");
+    		return;
+    	}
+
+    	//检查拓展名是否正确
+    	if(path.extname(files.studentexcel.name) != ".xlsx"){
+    		//删除这个不正确的文件
+    		fs.unlink("./" + files.studentexcel.path,function(err){
+    			if(err){
+    				console.log("删除文件错误");
+    				return;
+    			}
+    			res.send("对不起，上传的文件类型不正确，你上传的已经从服务器删除");
+    		});
+    		return;
+    	}
+
+    	// 读取这个Excel表格了，这是一条同步语句，所以没有回调函数
+    	//workSheetsFromFile是一个数组
+    	var workSheetsFromFile = xlsx.parse("./" + files.studentexcel.path);
+    	//检查数组是不是符合规范
+    	if(workSheetsFromFile.length != 6){
+    		res.send("系统检查到你的Excel表格缺少子表格");
+    		return;
+    	}
+    	//循环检查每个表的表头是否完整
+    	for(var i = 0 ; i < 6 ; i++){
+    		if(
+    			workSheetsFromFile[i].data[0][0] != "学号" ||
+    			workSheetsFromFile[i].data[0][1] != "姓名" 
+    		){
+    			res.send("系统检查到你的Excel表格" + i + "号子表的表头不正确，请保证6个年级的子表的表头都有“学号”、“姓名”");
+    			return;
+    		}
+    	}
+    	//至此，我们认为workSheetsFromFile数组是一个合法的数据了！
+    	//命令Mongoose将数据存储到数据库中！
+    	Student.importStudent(workSheetsFromFile);
+
+    	//输出
+    	res.send("上传成功！");
+    });
+}
+
+
+// //全部学生的数据，被jqGrid限制了API形式。
+// //并且这个接口是用GET请求发送来的
+// //如同： student?_search=false&nd=1490872998973&rows=2&page=1&sidx=sid&sord=asc
+// exports.getAllStudent = function(req,res){
+//     //拿到参数
+//     var rows = url.parse(req.url,true).query.rows;
+//     var page = url.parse(req.url,true).query.page;
+//     var sidx = url.parse(req.url,true).query.sidx;
+//     var sord = url.parse(req.url,true).query.sord;
+
+//     var sordNumber = sord == "asc" ? 1 : -1;
+
+//     //分页算法
+//     Student.count({},function(err,count){
+//         //总页数
+//         var total = Math.ceil(count / rows);
+//         //排序、分页
+//         //参考了博客：http://blog.csdn.net/zk437092645/article/details/9345885
+//         var sortobj = {};
+//         //动态绑定一个键
+//         sortobj[sidx] = sordNumber;
+//         //这是一个结合了排序、分页的大检索
+//         //为什么要暴露records、page、total、rows这些键，都是jqGrid要求的
+//         //请看     http://blog.mn886.net/jqGrid/ ，  左侧点击新手demo
+//         //它的API：http://blog.mn886.net/jqGrid/JSONData
+//         Student.find({}).sort(sortobj).limit(rows).skip(rows * (page - 1)).exec(function(err,results){
+//             res.json({"records" : count, "page" : page, "total" : total , "rows" : results});
+//         });
+//     });
+// }
